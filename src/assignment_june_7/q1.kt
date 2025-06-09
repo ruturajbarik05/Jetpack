84dc6eb6e883e20ec591083eec414913c64dada8
ruturajbarik05 <ruturajbarik05@gmail.com>
2025-06-09 15:08:38 +0530

    q1,q2 added

---
+++ src/assignment_june_7/q1.kt
@@ -0,0 +1,6 @@
+package assignment_june_7
+
+fun main(){
+    val coffeeshopname = "Starbuzz"
+    println(" Welcome to $coffeeshopname !")
+}
---
+++ src/assignment_june_7/q2.kt
@@ -0,0 +1,7 @@
+package assignment_june_7
+
+fun main(){
+    var favcoffee = "Espresso"
+    println("Your favorite coffee is $favcoffee")
+
+}
